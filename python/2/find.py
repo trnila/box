0856887e224aacc127ec42a2d60f19d13b147331
Daniel Trnka <daniel.trnka@gmail.com>
2019-07-03 15:52:34 +0200

    added second task

---
+++ python/2/find.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import glob
+
+p = argparse.ArgumentParser()
+p.add_argument('dir', help='Path to directory')
+p.add_argument('mask', help='file mask')
+
+args = p.parse_args()
+print("\n".join(glob.glob(os.path.join(args.dir, args.mask))))
