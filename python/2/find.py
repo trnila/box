086365cbb976f51ed0b5f9834135b4edd2953306
Daniel Trnka <daniel.trnka@gmail.com>
2019-07-03 15:54:45 +0200

    python/2: allow recursive match with **

--- python/2/find.py
+++ python/2/find.py
@@ -8,4 +8,4 @@ p.add_argument('dir', help='Path to directory')
 p.add_argument('mask', help='file mask')
 
 args = p.parse_args()
-print("\n".join(glob.glob(os.path.join(args.dir, args.mask))))
+print("\n".join(glob.glob(os.path.join(args.dir, args.mask), recursive=True)))
